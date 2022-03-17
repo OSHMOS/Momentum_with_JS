0b645a2b1975f0427b67abb0330444eb4f335e5c
OSHMOS <ojames9707@gmail.com>
2022-03-17 12:52:17 +0900

    Conditionals part two : if (condition) { condition === true} else {condition === false}

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // Conditionals
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
-console.log(typeof(age)); // to look type of variable
+console.log(isNaN(age));
+// false => age is number
+// true => age is not number
+// if (condition){
+//   // condition === true
+//   // condition has to be "boolean"
+// } else {
+//   // condition === false
+// }
+
+if (isNaN(age)) {
+  console.log("Please write a number");
+  prompt("How old are you?");
+} else {
+  console.log(`You are ${age} years old.`);
+}
+
+// not for ~ => read if ~ once only
