f87871bf411de934d4847866b10ec6ba7eef020e
OSHMOS <ojames9707@gmail.com>
2022-03-17 13:04:17 +0900

    Conditionals part three : else if, operator(&&, ||)

--- app.js
+++ app.js
@@ -11,11 +11,29 @@ console.log(isNaN(age));
 //   // condition === false
 // }
 
-if (isNaN(age)) {
-  console.log("Please write a number");
+// operator &&
+true && true === true
+false && true === true
+true && false === true
+false && false === false
+
+// operator ||
+true || true === true
+false || true === true
+true || false === true
+false || false === false
+
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
   prompt("How old are you?");
-} else {
-  console.log(`You are ${age} years old.`);
+} else if (age < 18) {
+  console.log("You're too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+}else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 }
 
 // not for ~ => read if ~ once only
