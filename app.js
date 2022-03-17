98854035d5659b2067823872d9c4df6966bb72e1
OSHMOS <ojames9707@gmail.com>
2022-03-17 11:46:36 +0900

    Function part one : function sayHello() {}

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-// Objects
-const player = {
-  name: 'osh',
-  points: 10,
-  fat: true,
-};
-console.log(player);
+// function
+function sayHello() {
+  console.log("Hello my name is ");
+}
 
-// Get property in Objects
-console.log(player.name);
-console.log(player['name']);
-// Player is constant but property is not constant
-player.fat = false;
-console.log(player);
-
-// Add property in Objects
-player.lastName = 'mos';
-console.log(player);
-
-player.points += 15;
-console.log(player.points);
+sayHello();
