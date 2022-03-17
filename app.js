263f89820422e739a8317fde43588ffdab9a9150
OSHMOS <ojames9707@gmail.com>
2022-03-17 13:09:36 +0900

    Condional recap : operator, condition order

--- app.js
+++ app.js
@@ -30,10 +30,15 @@ if (isNaN(age) || age < 0) {
   console.log("You're too young.");
 } else if (age >= 18 && age <= 50) {
   console.log("You can drink");
-}else if (age > 50 && age <= 80) {
+} else if (age > 50 && age <= 80) {
   console.log("You should exercise");
+} else if (age === 100) {
+  console.log("wow you are wise");
 } else if (age > 80) {
   console.log("You can do whatever you want.");
+} else if (age === 100) {
+  console.log("wow you are wise");
 }
 
+// JS => inner() => outer()
 // not for ~ => read if ~ once only
