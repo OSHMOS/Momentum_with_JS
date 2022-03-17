b2fa87944bb736be782f977c41cad12db198eaa7
OSHMOS <ojames9707@gmail.com>
2022-03-17 11:57:16 +0900

    Function part two : (data in arguments), other shape

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // function
-function sayHello() {
-  console.log("Hello my name is ");
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + ". I'm " + age + " years old");
 }
 
-sayHello();
+sayHello("oshmos", 26);
+sayHello("jsjsjsjsjs1019", 24);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+function devide(a, b) {
+  console.log(a / b);
+}
+
+plus(8, 60); // ordered >>
+devide(98, 20);
+
+const player = {
+  name: "osh",
+  sayHello: function (otherPersonName) {
+    console.log(`Hello my name is ${otherPersonName}.`);
+  }
+};
+
+console.log(player.name);
+player.sayHello("oshmos");
