1fbd34a9096512dc5e9e6f940b18448a1ff8500c
OSHMOS <ojames9707@gmail.com>
2022-03-18 09:50:28 +0900

    many solutions,, but best is querySelector

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const title = document.getElementById("title");
-title.innerText = "Got You!";
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+hellos.innerText = "hello"; // many elements are not changed
 
-console.log(title.id);
-console.log(title.className);
+const h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+const qs = document.querySelector(".hi h1"); // 99.98%
+console.log(qs);
+qs.innerText = "Hi!"; // change is in only one elements
