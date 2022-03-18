290198a4f61e8db1cfce08b9017618d001ef33da
OSHMOS <ojames9707@gmail.com>
2022-03-18 10:12:26 +0900

    only event listening with window

--- app.js
+++ app.js
@@ -12,6 +12,27 @@ function handleMouseLeave() {
   console.log("mouse is gone");
 }
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copy!");
+}
+
+function handleWindowOffline() {
+  alert("SOS. No WIFI!");
+}
+
+function handleWindowOnline() {
+  alert("WIFI is connected");
+}
+
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("online", handleWindowOnline);
+window.addEventListener("offline", handleWindowOffline);
