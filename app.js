121b57a006515375ce064185d62eec93b038df2a
OSHMOS <ojames9707@gmail.com>
2022-03-18 10:04:40 +0900

    addEventListener has many events

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ const title = document.querySelector(".hello h1");
 function handleTitleClick() {
   console.log("title was clicked!");
 }
+
+function handleMouseEnter() {
+  console.log("mouse is here!");
+}
+
+function handleMouseLeave() {
+  console.log("mouse is gone");
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
