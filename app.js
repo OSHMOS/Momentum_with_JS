25672add1402560c49e9998ae21defeaa9808eb4
OSHMOS <ojames9707@gmail.com>
2022-03-21 15:35:02 +0900

    CSS in JavaScript Part three, classList.remove(), add() => classList.toggle()

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const h1 = document.querySelector(".hello h1");
 
+// function handleTitleClick() {
+//   const clickedClass = "clicked"
+//   if (h1.classList.contains(clickedClass)) {
+//     h1.classList.remove(clickedClass);
+//   } else {
+//     h1.classList.add(clickedClass);
+//   }
+// }
+
 function handleTitleClick() {
-  const clickedClass = "clicked"
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
