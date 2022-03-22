17765d7a3ce79bdbfcc2221e26e4744da0284749
OSHMOS <ojames9707@gmail.com>
2022-03-22 10:23:02 +0900

    Form Submission

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
+  const userName = loginInput.value;
+  console.log(userName);
 }
 loginButton.addEventListener("click", onLoginBtnClick);
