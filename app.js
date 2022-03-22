a6f8ec990019174d3a3733e5b95d113decb40bbd
OSHMOS <ojames9707@gmail.com>
2022-03-22 10:39:23 +0900

    event.preventDefault(); : link, <a></a>, PointerEvent

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(e) {
   e.preventDefault();
   console.log(loginInput.value);
 }
+
+function handleLinkClick(e) {
+  e.preventDefault();
+  console.log(e);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
