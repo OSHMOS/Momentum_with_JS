f310220aeb9b80abc63aa83c6fad596845e17073
OSHMOS <ojames9707@gmail.com>
2022-03-22 10:17:41 +0900

    Input Values with JS

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const h1 = document.querySelector(".hello h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-// function handleTitleClick() {
-//   const clickedClass = "clicked"
-//   if (h1.classList.contains(clickedClass)) {
-//     h1.classList.remove(clickedClass);
-//   } else {
-//     h1.classList.add(clickedClass);
-//   }
-// }
-
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+  console.log(loginInput.value);
 }
-
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
