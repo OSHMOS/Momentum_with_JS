0937dbf8c2f99385b0aff9204c71c061041a90c4
oshmos <ojames9707@gmail.com>
2022-04-23 21:00:17 +0900

    Deleting toDos

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,28 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+function deleteToDo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span);
   span.innerHTML = newToDo;
+  const btn = document.createElement('button');
+  btn.innerHTML = 'X';
+  btn.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(e) {
   e.preventDefault();
-  const newToDo = toDoInput.ariaValueMax;
+  const newToDo = toDoInput.value;
   toDoInput.value = '';
   paintToDo(newToDo);
 }
 
-todoForm.addEventListener('submit', handleToDoSubmit);
+toDoForm.addEventListener('submit', handleToDoSubmit);
