8bfffe0ee6582cacec6b2db71a9e50095d4ae92c
oshmos <ojames9707@gmail.com>
2022-04-23 21:44:58 +0900

    Loading toDos : copy the localStorage

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.getElementById('todo-list');
 
 const TODOS_KEY = 'toDos';
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -33,13 +33,15 @@ function handleToDoSubmit(e) {
   toDoInput.value = '';
   toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if (savedToDos != null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log(item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
