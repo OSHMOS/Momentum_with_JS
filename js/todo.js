d0a0da959f4a31f3889f59668cd612f19e1a2af5
oshmos <ojames9707@gmail.com>
2022-04-23 20:39:56 +0900

    Adding toDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,18 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+function paintToDo(newToDo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  li.appendChild(span);
+  span.innerHTML = newToDo;
+}
+
 function handleToDoSubmit(e) {
   e.preventDefault();
   const newToDo = toDoInput.ariaValueMax;
   toDoInput.value = '';
+  paintToDo(newToDo);
 }
 
 todoForm.addEventListener('submit', handleToDoSubmit);
