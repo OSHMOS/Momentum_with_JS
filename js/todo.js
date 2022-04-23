daccd8aad9626ebae1f5580263561ab5631c8c51
oshmos <ojames9707@gmail.com>
2022-04-23 21:09:07 +0900

    Saving toDos : Saving array in localStorage

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem('toDos', JSON.stringify(toDos));
+}
+
 function deleteToDo(e) {
   const li = e.target.parentElement;
   li.remove();
@@ -23,6 +29,7 @@ function handleToDoSubmit(e) {
   e.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = '';
+  toDos.push(newToDo);
   paintToDo(newToDo);
 }
 
